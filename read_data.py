0908d38aea28d57d99d9c07c0b66d4702204db0b
Fade37 <76741177+Fade37@users.noreply.github.com>
2023-07-18 08:41:40 +0200

    Delete read_data.py

--- read_data.py
+++
@@ -1,7 +0,0 @@
-from TextPreprocessor import *
-
-text_preprocessor = TextPreprocessor()
-
-for i, j in enumerate(range(1976, 1987), 1):
-    text_preprocessor.text_to_excel(
-        filename=f"lehrerbedarf {i}.txt", excel_name=f"{j}")
